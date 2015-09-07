b1ab986d92af1d1363e9e92e327fa58b87dedbfd
Tomáš Hübelbauer <Tomas@Hubelbauer.net>
2015-09-07 10:14:53 +0200

    Create an empty UWP library and set assembly info

---
+++ src/Rhaeo.Stun/Rhaeo.Stun/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rhaeo.Stun")]
+[assembly: AssemblyDescription("The STUN (Session Traversal Utilities for NAT) implementation for UWP (Universal Windows Platform) as per RFC5389.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rhaeo")]
+[assembly: AssemblyProduct("Rhaeo.Stun")]
+[assembly: AssemblyCopyright("Copyright © Rhaeo 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
